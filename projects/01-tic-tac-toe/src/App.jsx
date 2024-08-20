9ef1bd6d590cd100e5010d6af3ff5524c303bb9b
BautiiBevi <bautistabevilacqua@hotmail.com>
2024-08-19 22:23:09 -0300

    Finishing project

--- projects/01-tic-tac-toe/src/App.jsx
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -1,24 +1,36 @@
 import { useState } from "react"
-
-const TURNS = {
-X : "x",
-O : "o"
-}
-
-
-const Square = ({children, updateBoard, index}) => {
-  return(
-    <div className="square">
-      {children}
-    </div>
-  )
-}
+import { Square } from "./components/Square.jsx"
+import{ TURNS} from "./constants.js"
+import { checkWinnerFrom, checkEndGame} from "./logic/board.js"
+import { WinnerModal } from "./components/WinnerModal.jsx"
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null)
+  
+  const resetGame = ()=>{
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
+
+  const updateBoard = (index)=>{
+    if(board[index] || winner) return
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    const newWinner = checkWinnerFrom(newBoard)
+    if (newWinner){
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)){
+      setWinner(false)
+    }
+  }
   return(
-
-
     <main className="board">
       <h1>Tic-Tac-Toe</h1>
       <section className="game">
@@ -28,13 +40,23 @@ function App() {
               <Square
                 key={index}
                 index = {index}
+                updateBoard={updateBoard}
               >
-              {index}
+              {board[index]}
               </Square>
             )
           })
         }
       </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>         
+      </section>
+      <WinnerModal resetGame={resetGame} winner={winner}/>
     </main>
   )
 }
